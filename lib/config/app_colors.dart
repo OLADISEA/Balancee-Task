25668b660f23db3e340ba782b76675f84421ff2d
OLADISEA <ladisea55@gmail.com>
2024-09-08 21:52:43 +0100

    Fix

--- lib/config/app_colors.dart
+++ lib/config/app_colors.dart
@@ -15,24 +15,5 @@ class AppColor{
 
 
 
-  static const Color kCanvasColor = Color(0xFFF5F7FB);
-  static const Color kLightBlack = Colors.black26;
-  static const Color kPrimaryColor = Color(0xFF17253D);
-  static const Color kBorderGrey = Color(0xFFCBD5E1);
-  static const Color kLightPrimaryColor = Color(0xFFD9DFEF);
-  static const Color kVeryLightBlueColor = Color(0xFFA4AEC4);
-  static const Color kLightBlueColor = Color(0xFF7281A0);
-  static const Color kAccentColor = Color(0xFFFF9800);
-  static const Color kLightTextColor = Color(0xFFEEF0F4);
-  static const Color kBlueOneVariantColor = Color(0xFF9DA4A4);
-  static const Color kBlueTwoVariantColor = Color(0xFF686C6C);
-  static const Color kBlueThreeVariantColor = Color(0xFF5c749a);
-  static const Color kDividerColor = Color(0xFFBDBDBD);
-  static const Color kRed = Color(0xFF960000);
-  static const Color kGreen = Color(0xFF285D34);
-  static const Color kTransparent = Colors.transparent;
-  static const Color kDeepTextColor = Color(0xFF334155);
-  static const Color kIconColor = Color(0xFF475569);
-
 
 }
